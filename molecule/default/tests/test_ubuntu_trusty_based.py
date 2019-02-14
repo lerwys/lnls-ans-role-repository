a3cce01ab3bc5d8ee9bdf0992197563d54232340
Lucas Russo <lerwys@gmail.com>
2019-02-13 23:29:22 -0200

    molecule/*/tests: add pinning repo tests for Ubuntu Trusty

--- molecule/default/tests/test_ubuntu_trusty_based.py
+++ molecule/default/tests/test_ubuntu_trusty_based.py
@@ -16,3 +16,14 @@ def test_repos(host, repo):
     cmd = host.run('apt-cache policy | grep http | awk \'{print $2,$3}\' | sort -u')
 
     assert repo in cmd.stdout
+
+
+@pytest.mark.parametrize('pkg_pin', [
+    ('libpcre3', 'deb8'),
+    ('epics-dev', '3.15'),
+    ('epics-perl', 'none')
+])
+def test_pkg_pin(host, pkg_pin):
+    cmd = host.run('apt-cache policy %s | grep "Candidate:" | sed -e \'s/Candidate://g\''.format(pkg_pin[0]))
+
+    assert pkg_pin[1] in cmd.stdout
